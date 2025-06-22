b7b965c7faede9a8d01e4e6b4546793f075d42a6
Ilgaz Aydin <ilgazaydin@gmail.com>
2025-06-22 22:33:22 +0100

    remove api request timeout

--- src/lib/api/app.ts
+++ src/lib/api/app.ts
@@ -17,7 +17,7 @@ const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 export const api = axios.create({
   baseURL,
-  timeout: 10000,
+  // timeout: 10000,
   headers: {
     "Content-Type": "application/json",
   },
